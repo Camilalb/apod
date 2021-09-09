d672beb833e51ce91954d70403b541e8f8cbfb43
Camilalb <milafviana@gmail.com>
2021-09-09 17:21:39 -0300

    Inicio do projeto

---
+++ index.js
@@ -0,0 +1,6 @@
+// Criação de variáveis para pegar info do HTML
+
+let btnNode = document.getElementById('elemBtn')
+let dateNode = document.getElementById('')
+let imagemNode = document.getElementById('elemimagem')
+let legendaNode = document.getElementById('elemlegenda')
